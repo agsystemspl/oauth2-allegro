efb8165c4154ff86ff546cf089f7aadb53b184fc
Paweł Feruś <panther.mindc@gmail.com>
2018-12-21 12:39:31 +0100

    remove interface

--- src/Token/AccessTokenInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace AGSystems\OAuth2\Client\Token;
-
-interface AccessTokenInterface extends \League\OAuth2\Client\Token\AccessTokenInterface
-{
-    public function saveAuth(AccessTokenInterface $accessToken);
-}
